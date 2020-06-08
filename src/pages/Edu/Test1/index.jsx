a593604dd6d0dbd721907abba9840a71987faee9
xiaobo <164419221@qq.com>
2020-06-08 12:01:34 +0800

    ''

---
+++ src/pages/Edu/Test1/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+    render() {
+        return (
+            <div>
+                Test.....
+            </div>
+        );
+    }
+}
+
+export default index;
